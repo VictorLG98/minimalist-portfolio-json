824bb3f9629e5c014638e2127e76125550d92240
victor <vlozoyag01gmail.com>
2026-02-27 15:59:04 +0100

    nuevos cambios

--- src/lib/cv.ts
+++ src/lib/cv.ts
@@ -1,14 +1,14 @@
-import type { CV } from "../cv"
-
 import cvEs from "../../cv.json"
 import cvEn from "../../cv_english.json"
 
 export type Lang = "es" | "en"
 
-const CVS: Record<Lang, CV> = {
-  es: cvEs as CV,
-  en: cvEn as CV,
-}
+const CVS = {
+  es: cvEs,
+  en: cvEn,
+} as const
+
+export type CV = typeof cvEs
 
 export const getCV = (lang: Lang): CV => CVS[lang]
 
